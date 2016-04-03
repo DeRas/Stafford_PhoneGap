422f10554989ecb4bc1897bc266d932085614598
Dennis Rasmussen <dennis@deras.dk>
2016-04-03 16:01:21 +0200

    Fixed standard errors

--- Stafford/www/scripts/ApiCon.js
+++ Stafford/www/scripts/ApiCon.js
@@ -1,14 +1,22 @@
 ﻿$(document).ready(function () {
-    $.ajax({
-        url: "http://rest-service.guides.spring.io/greeting"
-    }).then(function (data) {
-        $('.greeting-id').append(data.id);
-        $('.greeting-content').append(data.content);
-    });
+
+    //$.ajax({
+    //    url: "http://staffy.dk/api/images"
+    //}).then(function (data){
+    //    $('.greeting-id').append(data[0].UrlToImage);
+    //});
+
+    //$.ajax({
+    //    url: "http://rest-service.guides.spring.io/greeting"
+    //}).then(function (data) {
+    //    $('.greeting-id').append(data.id);
+    //    $('.greeting-content').append(data.content);
+    //});
+
 });
 
 
 
-$(function () {
-    $("#penis").load("Test.html");
-});
+//$(function () {
+//    $("#penis").load("Test.html");
+//});
